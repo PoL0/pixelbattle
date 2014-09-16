b8bd2991b109ed89a5ddefecaf6cc6aed30b3595
Trevor Westphal <twestpha@email.com>
2014-09-15 22:21:42 -0600

    Added a rotating sprite... lots to do

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Image.hpp>
+#include <stdio.h>
 
 using namespace sf;
 
 int main()
 {
-    RenderWindow window(sf::VideoMode(1024, 768), "SFML works!");
+    RenderWindow window(sf::VideoMode(1024, 768), "");
+    
+    sf::Texture texture;
+    if (!texture.loadFromFile("sprites/GreenDarknut.png")){
+        printf("Shit");
+    }
+
+    Sprite sprite;
+    sprite.setTexture(texture);
+
+    sprite.setPosition(Vector2f(60, 60));
 
     while (window.isOpen()){
 
@@ -16,9 +28,13 @@ int main()
                 window.close();
         }
 
+        texture.setSmooth(true);
+        sprite.rotate(1);
+
         // frame cleanup
         // TODO - modify to draw when it needs to instead of always
         window.clear();
+        window.draw(sprite);
         window.display();
     }
 
